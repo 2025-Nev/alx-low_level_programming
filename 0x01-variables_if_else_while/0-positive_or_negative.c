decf5882f32be0edd835889e00a66dcc9e00e2ed
2025-Nev <nevillenyax@gmail.com>
2022-09-09 07:07:01 -0700

    git commit -m "0-positivee_or_negative.c"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -27,14 +27,18 @@ int main(void)
 	printf("%i is zero\n", n);
 	}
        
-	else if (n < 0)
+	else if (n < 0) 
+
 	{
-	printf("%i is negative\n", n);
+	
+	 printf("%i is negative\n", n);
 	}
+
 	 else 
 	 {
-          printf("%i is positive\n", n);
+		 printf("%i is positive\n", n);
 	 }
+
 	return (0);
 
 }
