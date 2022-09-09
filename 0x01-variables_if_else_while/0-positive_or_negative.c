0da6d6d0c9019d37d825b4648328444d18c21ce5
2025-Nev <nevillenyax@gmail.com>
2022-09-09 06:38:05 -0700

    git commit -m "0_positive_or_negative"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,18 +23,18 @@ int main(void)
 				n = rand() - RAND_MAX / 2;
 
 		                if (n == 0)
-{
+	{	
 	printf("%i is zero\n", n);
-}
+	}
        
-else if (n < 0)
-{
+	else if (n < 0)
+	{
 	printf("%i is negative\n", n);
-}
+	}
 	 else 
 	 {
           printf("%i is positive\n", n);
 	 }
-return (0);
+	return (0);
 
 }
