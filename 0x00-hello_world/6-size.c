e11ef0b3df6511372ffa380a32608a4640f59c46
2025-Nev <nevillenyax@gmail.com>
2022-09-11 16:12:58 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- * * main - Prints the sizeof many var types
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+ * main - Prints the size of various types
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
