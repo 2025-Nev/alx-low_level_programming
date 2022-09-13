3cf149a805858b5b6ae4b6c576f99f74dfd6009f
2025-Nev <nevillenyax@gmail.com>
2022-09-13 13:07:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('u'');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
@@ -17,7 +17,7 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
+	return (0) ;
 }
 
 
