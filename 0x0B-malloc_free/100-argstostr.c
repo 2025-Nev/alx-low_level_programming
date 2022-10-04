acf7af21ab17339371b1055185a5295bd16916b3
2025-Nev <nevillenyax@gmail.com>
2022-10-04 01:32:18 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * len -  returns length of str
+ * @str: string counted
+ * Return: returns the length
+ */
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
+}
+
