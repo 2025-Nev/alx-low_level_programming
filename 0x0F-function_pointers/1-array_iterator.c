67fc8a64163d380368ddb689b13f019431c9e520
2025-Nev <nevillenyax@gmail.com>
2022-10-14 15:11:47 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @size: size of the array.
  * @action: pointer to the function.
  */
-void array_iteraor(int *array, size_t size, void(*action) (int))
+void array_iteraor(int *array, size_t size, void (*action) (int))
 {
 	unsigned int i;
 
