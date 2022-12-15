7694fab7a9836650f63a8f686e1cd34be76e3807
2025-Nev <nevillenyax@gmail.com>
2022-12-15 04:42:22 -0800

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+Int add(int a, int b)
+
+{
+
+      
+
+      return (a + b);
+
+
+
+}
+
+
+
+Int add(int a, int b)
+
+{
+
+      
+
+      return (a - b);
+
+
+
+}
+
+
+
+
+
+Int add(int a, int b)
+
+{
+
+      
+
+      return (a * b);
+
+
+
+}
+
+
+
+
+
+Int add(int a, int b)
+
+{
+
+      
+
+      return (a / b);
+
+
+
+}
