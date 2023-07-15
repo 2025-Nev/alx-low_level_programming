1b8a3dbff72bb911ce0b93e44327acf00a268ea1
2025-Nev <nevillenyax@gmail.com>
2023-07-15 10:13:57 -0700

    search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCHING_ALGORITHM_H
+#define SEARCHING_ALGORITHM_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binarySearch(int arr[], int l, int r, int x);
+int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
